0504a96a7cfe3ae9a14ffb5c90ed175622d6ffc6
Isaac Torres Espuña <isaac@ite2002.net>
2026-01-01 16:06:29 +0100

    Created dashboard boilerplate

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { Dashboard} from './dashboard/dashboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Dashboard],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('metabric-uoc-viz');
-}
+export class App {}
